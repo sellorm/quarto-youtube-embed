8558197e9a9ac5331b7967c00ab3bbdc710a77c2
Mark Sellors <github@5vcc.com>
2022-08-08 13:31:59 +0100

    tweaks

--- _extensions/youtube-embed/youtube.lua
+++ _extensions/youtube-embed/youtube.lua
@@ -1,5 +1,5 @@
-function youtube(shortcode)
+function youtube(videoid)
     if quarto.doc.isFormat('html') then
-        return pandoc.Str('<iframe width="1087" height="611" src="https://www.youtube.com/embed/zk9zHqXm6m0"' .. shortcode .. 'frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+        return pandoc.Str('<iframe width="1087" height="611" src="https://www.youtube.com/embed/' .. videoid .. '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
     end
 end
