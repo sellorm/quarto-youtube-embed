6f91b7e43d708fdd660466c067a5b8e23262dab2
Mark Sellors <github@5vcc.com>
2022-08-12 14:48:16 +0100

    Tidying

--- _extensions/youtube/youtube.lua
+++ _extensions/youtube/youtube.lua
@@ -1,8 +1,13 @@
 function youtube(args)
     local videoid = pandoc.utils.stringify(args[1])
-    local html = '<div id="youtube-frame" style="position: relative; padding-bottom: 56.25%; /* 16:9 */ height: 0;"><iframe width="100%" height="" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/' .. videoid .. '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+
+    -- Assemble HTML to be returned
+    local html = '<div id="youtube-frame" style="position: relative; padding-bottom: 56.25%; /* 16:9 */ height: 0;"><iframe width="100%" height="" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/'
+        .. videoid
+        .. '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+
     if quarto.doc.isFormat('html') then
-	return pandoc.RawInline('html', html)
+	    return pandoc.RawInline('html', html)
     else
         return pandoc.Null()
     end
